865fa7c2b5094e59ccacbd16581da39bc5f76854
yoggy <yoggy0@gmail.com>
2013-05-18 18:41:51 +0900

    Initial commit

---
+++ led_weather_indicator.ino
@@ -0,0 +1,69 @@
+//
+//  led_weather_indicator.ino
+//
+//  This sketch is using Adafruit_NeoPixel library.
+//    https://github.com/adafruit/Adafruit_NeoPixel
+//
+#include <Adafruit_NeoPixel.h>
+
+#define LED_NUM 8
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_NUM, 6, NEO_GRB + NEO_KHZ800);
+
+byte *buf = new byte[LED_NUM];
+uint16_t buf_idx = 0;
+
+uint32_t color100 = strip.Color(12,  6,  0); // sunny
+uint32_t color200 = strip.Color( 6,  6,  6); // cloudy
+uint32_t color300 = strip.Color( 0,  0, 12); // rainy
+uint32_t color400 = strip.Color( 6,  6, 12); // snowy
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial); // for leonald, micro...
+  
+  strip.begin();
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    int c = Serial.read();
+    Serial.println(c);
+    if (c == 'c') {
+      buf_idx = 0;
+    }
+    else {
+      buf[buf_idx] = c;
+      buf_idx ++;
+      if (buf_idx == 8) {
+        set_led_color();
+        buf_idx = 0;
+      }
+    }
+  }
+}
+
+void set_led_color() {
+  for(uint16_t i = 0; i < strip.numPixels(); ++i) {
+    switch (buf[i]) {
+      case '1':
+        strip.setPixelColor(i, color100);
+        break;
+      case '2':
+        strip.setPixelColor(i, color200);
+        break;
+      case '3':
+        strip.setPixelColor(i, color300);
+        break;
+      case '4':
+        strip.setPixelColor(i, color400);
+        break;
+      default:
+        strip.setPixelColor(i, strip.Color( 0,  0, 0));
+        break;
+    }
+  }
+  strip.show();
+}
+
+
---
+++ led_weather_indicator.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/ruby
+#
+#  led_weather_indicator.rb 
+#
+
+require 'rubygems'
+require 'serialport'
+require 'open-uri'
+require 'rexml/document'
+require 'pp'
+
+url = "http://weathernews.jp/pinpoint/xml/46106.xml" # 横浜
+dev = "/dev/usbserial"
+log_file = File.dirname($0) + "/weather.log"
+
+f = open(log_file, "a+")
+
+f.puts("==== date : " + Time.now.strftime("%Y-%m-%d %H:%M:%S"))
+
+# 1時間毎の天気コードを取得
+doc = REXML::Document.new(open(url))
+codes = [];
+doc.elements.each('weathernews/data/day/weather/hour') {|e|
+  codes << e.text.to_i
+}
+codes = codes.slice(0, 24) #32時間分あるので先頭24時間分だけ取り出す
+f.puts "weather codes=#{codes.join(',')}"
+
+# 天気コード比較
+def code_rank(a)
+  rank = {
+      0 => 0,
+    100 => 1,
+    800 => 2,
+    200 => 3,
+    300 => 4,
+    400 => 5,
+  }
+  return rank[a] if rank.key?(a)
+  
+  return 10
+end
+
+def compare_code(a, b)
+  puts 
+  return code_rank(a) <= code_rank(b)
+end
+
+# 3時間毎の天気コードに集約
+codes_e3h = []
+count = 0;
+max_code = 0;
+codes.each {|c|
+  if compare_code(max_code ,c)
+    max_code = c
+  end
+  count += 1
+  if count == 3
+    codes_e3h << max_code
+    max_code = 0
+    count = 0
+  end
+}
+f.puts "weather codes (every 3 hours)=" + codes_e3h.join(',')
+
+# arduinoに送信するコードを作成
+cmd = "c"
+codes_e3h.each {|c|
+  cmd << (c/100).to_s
+}
+f.puts "send command=" + cmd
+
+# arduinoへコードを送信 (leonald対策でsleepを入れている)
+sp = SerialPort.new(dev, 9600)
+sleep 2
+sp.write(cmd)
+sp.flush
+sleep 2
+sp.close
+
